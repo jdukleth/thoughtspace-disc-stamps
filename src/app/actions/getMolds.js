7f6c7f8c80cfee7a9577b9c980e6a9b5bc60096e
jdukleth <jason@dukleth.com>
2024-04-21 03:17:58 -0500

    adds accordion structure and TSA molds data

---
+++ src/app/actions/getMolds.js
@@ -0,0 +1,160 @@
+export const molds = {
+  distanceDrivers: [
+    {
+      mold: 'Animus',
+      speed: 11,
+      glide: 5,
+      turn: 0,
+      fade: 2,
+      stamps: []
+    },
+
+    {
+      mold: 'Construct',
+      speed: 10,
+      glide: 6,
+      turn: -1,
+      fade: 2,
+      stamps: []
+    },
+
+    {
+      mold: 'Synapse',
+      speed: 12,
+      glide: 5,
+      turn: 0,
+      fade: 3,
+      stamps: []
+    }
+  ],
+
+  fairwayDrivers: [
+    {
+      mold: 'Coalesce',
+      speed: 9,
+      glide: 5,
+      turn: 0,
+      fade: 3,
+      stamps: []
+    },
+
+    {
+      mold: 'Mantra',
+      speed: 9,
+      glide: 6,
+      turn: -2,
+      fade: 1,
+      stamps: []
+    },
+
+    {
+      mold: 'Nuance',
+      speed: 7,
+      glide: 5,
+      turn: -2,
+      fade: 1,
+      comingSoon: true,
+      stamps: []
+    },
+
+    {
+      mold: 'Omen',
+      speed: 9,
+      glide: 4,
+      turn: 0,
+      fade: 4,
+      stamps: []
+    },
+
+    {
+      mold: 'Votum',
+      speed: 7,
+      glide: 5,
+      turn: 0,
+      fade: 3,
+      stamps: []
+    },
+  ],
+
+  midranges: [
+    {
+      mold: 'Crux',
+      speed: 5,
+      glide: 4,
+      turn: 0,
+      fade: 2,
+      comingSoon: true,
+      stamps: []
+    },
+
+    {
+      mold: 'Mana',
+      speed: 5,
+      glide: 5,
+      turn: -2,
+      fade: 1,
+      stamps: []
+    },
+
+    {
+      mold: 'Pathfinder',
+      speed: 5,
+      glide: 5,
+      turn: 0,
+      fade: 1,
+      stamps: [],
+    },
+  ],
+
+  putters: [
+    {
+      mold: 'Alter',
+      speed: 3,
+      glide: 3,
+      turn: 0,
+      fade: 1,
+      bead: true,
+      stamps: []
+    },
+
+    {
+      mold: 'Muse',
+      speed: 3,
+      glide: 3,
+      turn: 0,
+      fade: 2,
+      bead: false,
+      stamps: []
+    },
+
+    {
+      mold: 'Pneuma',
+      speed: 2,
+      glide: 3,
+      turn: 0,
+      fade: 0,
+      bead: true,
+      stamps: []
+    },
+
+    {
+      mold: 'Praxis',
+      speed: 3,
+      glide: 3,
+      turn: 0,
+      fade: 1,
+      bead: false,
+      stamps: []
+    },
+
+    {
+      mold: 'Temple',
+      speed: 4,
+      glide: 3,
+      turn: 0,
+      fade: 3,
+      bead: false,
+      stamps: []
+    },
+  ],
+}
