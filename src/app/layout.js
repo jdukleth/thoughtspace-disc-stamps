8d8766aa15a723475a003e9b24936e5abee34df5
jdukleth <jason@dukleth.com>
2024-04-21 03:26:28 -0500

    lints default Next.js project code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,19 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Thoughtspace Disc Stamps",
-  description: "A collection of disc golf disc stamps for Thoughtspace Athletics molds",
-};
+  title: 'Thoughtspace Disc Stamps',
+  description: 'A collection of disc golf disc stamps for Thoughtspace Athletics molds',
+}
 
-export default function RootLayout({ children }) {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
+
+export default RootLayout
